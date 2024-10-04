e4e4c36215bd1878c04ae4b79e7962fb7c86aff2
Chris Nicoll <5599856+clutterstack@users.noreply.github.com>
2024-10-04 12:00:59 -0400

    put dev log level back to info

--- config/config.exs
+++ config/config.exs
@@ -48,7 +48,7 @@ config :tailwind,
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id],
-  level: :warning
+  level: :info
 
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
